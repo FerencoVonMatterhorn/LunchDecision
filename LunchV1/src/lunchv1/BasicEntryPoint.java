2182771e33fccc122a4fccbeb6a6d73868ca370d
horvayF <horvayF@422750-N01.marksys.de>
2018-08-16 17:02:48 +0200

    test
    Signed-off-by: horvayF <horvayF@422750-N01.marksys.de>

--- LunchV1/src/lunchv1/BasicEntryPoint.java
+++ LunchV1/src/lunchv1/BasicEntryPoint.java
@@ -66,7 +66,7 @@ public class BasicEntryPoint implements EntryPoint {
 		ToolItem calcitem = new ToolItem(dieToolbar, SWT.PUSH);
 		calcitem.setText("Calculator");
 //		dieToolbar.addListener(SWT.Dispose, evt -> lunchitem.dispose()); 
-//		Hier muss noch geschaut werden, was genau diese Zeile macht und ob sie zwingend notwendig ist
+		Hier muss noch geschaut werden, was genau diese Zeile macht und ob sie zwingend notwendig ist
 
 		return result;
 	}
