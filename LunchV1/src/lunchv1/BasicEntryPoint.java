24789c529878cac82ca03d2741fcf7f3d8a28dee
Ferenco <ferencs.ipod@gmx.de>
2018-08-21 11:17:24 +0200

    UI Changes

--- LunchV1/src/lunchv1/BasicEntryPoint.java
+++ LunchV1/src/lunchv1/BasicEntryPoint.java
@@ -4,8 +4,6 @@ import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.rap.rwt.application.EntryPoint;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridLayout;
@@ -14,6 +12,7 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 
@@ -35,175 +34,89 @@ public class BasicEntryPoint implements EntryPoint {
 
 	private Composite createLayer(Composite parent) {
 		parent.setLayout(new GridLayout(1, false));
-		Composite logolayer = createLogoLayer(parent);
-		GridDataFactory.fillDefaults().grab(false, false).align(SWT.CENTER, SWT.CENTER).applyTo(logolayer);
 
 		Composite toolbarlayer = createToolBarLayer(parent);
 		GridDataFactory.fillDefaults().grab(true, false).applyTo(toolbarlayer);
 
-		Composite AnwendungslayerOutSide = createAnwendungslayerOutSide(parent);
-		GridDataFactory.fillDefaults().grab(true, true).align(SWT.CENTER, SWT.CENTER).applyTo(AnwendungslayerOutSide);
+		Composite logolayer = createLogoLayer(parent);
+		GridDataFactory.fillDefaults().grab(false, false).align(SWT.CENTER, SWT.CENTER).applyTo(logolayer);
+
+		Composite searchbarlayer = createSearchBarLayer(parent);
+//		GridDataFactory.fillDefaults().grab(true, true).applyTo(searchbarlayer);
+
+		Composite Anwendungslayer = createAnwendungslayer(parent);
+		GridDataFactory.fillDefaults().grab(true, true).align(SWT.CENTER, SWT.CENTER).applyTo(Anwendungslayer);
 
 		return parent;
 	}
 
+//--------------------------------Toolbarlayer-------------------------------------------------------------------------------------------------------------
+
+	private Composite createToolBarLayer(Composite parent) {
+		Composite result = new Composite(parent, SWT.NONE);
+		result.setLayout(new FillLayout());
+
+		ToolBar dieToolbar = new ToolBar(result, SWT.HORIZONTAL);
+		ToolItem lunchitem = new ToolItem(dieToolbar, SWT.PUSH);
+		lunchitem.setText("LunchFinder");
+		new ToolItem(dieToolbar, SWT.SEPARATOR);
+		ToolItem votingItem = new ToolItem(dieToolbar, SWT.PUSH);
+		votingItem.setText("Voting");
+		new ToolItem(dieToolbar, SWT.SEPARATOR);
+		ToolItem kanPlan = new ToolItem(dieToolbar, SWT.PUSH);
+		kanPlan.setText("Wochenplan Kantine");
+		dieToolbar.addListener(SWT.Dispose, evt -> lunchitem.dispose());
+
+		return result;
+	}
+
 //----------------------------------LogoLayer---------------------------------------------------------------------------------------------------------------
-	
+
 	private Composite createLogoLayer(Composite parent) {
 		Composite result = new Composite(parent, SWT.NONE);
 		result.setLayout(new FillLayout());
-		
+
 		Label logo = new Label(result, SWT.NONE);
-		Image image = new Image(result.getDisplay(), "/home/ferenc/git/LunchDecision/LunchV1/res/Test.png");
+		Image image = new Image(result.getDisplay(), "Test.png");
 		logo.setImage(image);
 		logo.addListener(SWT.Dispose, evt -> image.dispose());
 		return result;
 	}
 
-//--------------------------------Toolbarlayer-------------------------------------------------------------------------------------------------------------
-	
-	private Composite createToolBarLayer(Composite parent) {
-		Composite result = new Composite(parent, SWT.NONE);
+//--------------------------------------Searchbar-----------------------------------------------------------------------------------------------------------
+
+	private Composite createSearchBarLayer(Composite parent) {
+		Composite result = new Composite(parent, SWT.BORDER);
 		result.setLayout(new FillLayout());
-		
-		ToolBar dieToolbar = new ToolBar(result, SWT.HORIZONTAL);
-		ToolItem lunchitem = new ToolItem(dieToolbar, SWT.PUSH);
-		lunchitem.setText("Lunch Application");
-		new ToolItem(dieToolbar, SWT.SEPARATOR);
-		ToolItem calcitem = new ToolItem(dieToolbar, SWT.PUSH);
-		calcitem.setText("Calculator");
-		dieToolbar.addListener(SWT.Dispose, evt -> lunchitem.dispose());
-//		Hier muss noch geschaut werden, was genau diese Zeile macht und ob sie zwingend notwendig ist
-		
+
+		GridDataFactory.fillDefaults().grab(true, false).align(SWT.CENTER, SWT.CENTER).applyTo(result);
+		Text dieSearchbar = new Text(result, SWT.BORDER);
+		Button derGoButton = new Button(result, SWT.BORDER);
+		derGoButton.setText("GO!");
+		derGoButton.addListener(SWT.Selection, (event) -> {
+			String searchtext = dieSearchbar.getText();
+			browser.setUrl("https://wego.here.com/search/" + searchtext + "?map=48.47265,7.92901,15,satellite");
+		});
+		dieSearchbar.addListener(SWT.DefaultSelection, (event) -> {
+			String searchtext = dieSearchbar.getText();
+			browser.setUrl("https://wego.here.com/search/" + searchtext + "?map=48.47265,7.92901,15,satellite");
+		});
 		return result;
 	}
-	
+
 //-----------------------------------AnwendunsLayer---------------------------------------------------------------------------------------------------------
 
-	private Composite createAnwendungslayerOutSide(Composite parent) {
-		Composite result = new Composite(parent, SWT.NONE);
-		result.setLayout(new GridLayout(2, true));
-		
-		Composite anwendungslayerLinks = createAnwendungsLayerLinks(result);
-		GridDataFactory.fillDefaults().grab(true, true).applyTo(anwendungslayerLinks);
-		
-		Composite anwendungslayerRechts = createAnwendungsLayerRechts(result);
-		GridDataFactory.fillDefaults().grab(true, true).applyTo(anwendungslayerRechts);
-		
-		return parent;
-	}
-
-	
-	private Composite createAnwendungsLayerLinks(Composite parent) {
+	private Composite createAnwendungslayer(Composite parent) {
 		Composite result = new Composite(parent, SWT.NONE);
 		result.setLayout(new GridLayout(1, true));
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(result);
 
-		Label nationalität = new Label(result, SWT.NONE);
-		nationalität.setText("Nach welchem Essen möchten sie Suchen ?");
-		radioButtonsErstellen(result);
-
-		return parent;
-	}
-
-	private Composite createAnwendungsLayerRechts(Composite parent) {
-		Composite result = new Composite(parent, SWT.NONE);
-		result.setLayout(new GridLayout(1, true));
-		GridDataFactory.fillDefaults().grab(true, true).applyTo(result);
-
-		//Button dertestBTN = new Button(result, SWT.BORDER);
-		//dertestBTN.eText("Hier solte etwas stehen lellelelelel");
 		browser = new Browser(result, SWT.BORDER); // Uses IE on MS
-        browser.setUrl("https://wego.here.com/?map=48.47265,7.92901,15,satellite");
-        
-        
-        GridDataFactory.fillDefaults().grab(true, true).applyTo(browser);
-        
-        Label listeessen = new Label(result, SWT.BORDER);
-        listeessen.setText("Hier sollte dann mal was essbares stehen");
-        return parent;
-	}
-	
+		browser.setUrl("https://wego.here.com/?map=48.47265,7.92901,15,satellite");
 
-//--------------------------------------Buttons----------------------------------------------------------------------------------------------
+		GridDataFactory.fillDefaults().grab(true, true).applyTo(browser);
 
-	private void radioButtonsErstellen(Composite result) {
-		Button alleAnzeigen = new Button(result, SWT.BORDER);
-		Button pizzaUndPastaAnzeigen = new Button(result, SWT.BORDER);
-		Button asiatischAnzeigen = new Button(result, SWT.BORDER);
-		Button indischAnzeigen = new Button(result, SWT.BORDER);
-		Button dönerAnzeigen = new Button(result, SWT.BORDER);
-		Button burgerUndCoAnzeigen = new Button(result, SWT.BORDER);
-		Button andereAnzeigen = new Button(result, SWT.BORDER);
-		Button randomeAnzeigen = new Button(result, SWT.BORDER);
-		//Button auswerten = new Button(result, SWT.BORDER);
-
-		//auswerten.setText("Auswerten");
-		alleAnzeigen.setText("Alle");
-		pizzaUndPastaAnzeigen.setText("Pizza & Pasta");
-		asiatischAnzeigen.setText("Asiatisch");
-		indischAnzeigen.setText("Indisch");
-		dönerAnzeigen.setText("Döner");
-		burgerUndCoAnzeigen.setText("Burger & Co");
-		andereAnzeigen.setText("Andere");
-		randomeAnzeigen.setText("Randooooooooooooomeee");
-
-		listenerAnButtonsHängen(alleAnzeigen, pizzaUndPastaAnzeigen, asiatischAnzeigen, indischAnzeigen, dönerAnzeigen,
-				burgerUndCoAnzeigen, andereAnzeigen, randomeAnzeigen);
+		return parent;
 	}
 
-	
-	
-	private void listenerAnButtonsHängen(Button alleAnzeigen, Button pizzaUndPastaAnzeigen, Button asiatischAnzeigen,
-			Button indischAnzeigen, Button dönerAnzeigen, Button burgerUndCoAnzeigen, Button andereAnzeigen,
-			Button randomeAnzeigen) {
-		alleAnzeigen.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				System.out.println("Ausgewählt");
-				browser.setUrl("https://wego.here.com/search/restaurant?map=48.47335,7.93724,15,satellite");
-			}
-		});
-		pizzaUndPastaAnzeigen.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				System.out.println("Ausgewählt");
-				browser.setUrl("https://wego.here.com/search/Pizza?map=48.47335,7.93724,15,satellite");
-			}
-		});
-		asiatischAnzeigen.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				System.out.println("Ausgewählt");
-				browser.setUrl("https://wego.here.com/search/Asiatisch?map=48.47335,7.93724,15,satellite");
-			}
-		});
-		indischAnzeigen.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				System.out.println("Ausgewählt");
-				browser.setUrl("https://wego.here.com/search/indisch?map=48.47335,7.93724,15,satellite");
-			}
-		});
-		dönerAnzeigen.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				System.out.println("Ausgewählt");
-				browser.setUrl("https://wego.here.com/search/döner?map=48.47335,7.93724,15,satellite");
-			}
-		});
-		burgerUndCoAnzeigen.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				System.out.println("Ausgewählt");
-				browser.setUrl("https://wego.here.com/search/burger?map=48.47335,7.93724,15,satellite");
-			}
-		});
-		andereAnzeigen.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				System.out.println("Ausgewählt");
-				browser.setUrl("https://wego.here.com/search/andere?map=48.47335,7.93724,15,satellite");
-			}
-		});
-		randomeAnzeigen.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				System.out.println("Ausgewählt");
-				browser.setUrl("https://wego.here.com/search/randome?map=48.47335,7.93724,15,satellite");
-			}
-		});
-	}
 }
